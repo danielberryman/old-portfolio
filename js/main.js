9162d76180522ad5cd966a5e3fcc64e54a989fb0
Daniel Berryman <danielberryman90@gmail.com>
2021-06-13 15:12:48 -0400

    styling for various screen sizes

--- js/main.js
+++ js/main.js
@@ -1,25 +1,42 @@
-var viewportWidth = window.innerWidth;
+window.addEventListener('resize', updateNav);
 
+var navIconContainer = document.getElementById('nav-icon-container');
 var hamburger = document.getElementById('hamburger');
 var exHamburger = document.getElementById('ex-hamburger');
 var nav = document.querySelector('nav');
 
-hamburger.onclick = toggleNavIcon;
-exHamburger.onclick = toggleNavIcon;
-if (window.innerWidth > 678) {
-  nav.classList.remove('hidden');
+navIconContainer.onclick = toggleNavIcon;
+
+if (window.innerWidth > 719) {
+  navIconContainer.style.display = 'none';
+  nav.style.display = 'inline';
 }
 
 function toggleNavIcon() {
-  toggleClass(hamburger, 'hidden');
-  toggleClass(exHamburger, 'hidden');
-  toggleClass(nav, 'hidden');
+  if (hamburger.style.display == 'none') {
+    exHamburger.style.display = 'none';
+    hamburger.style.display = 'block';
+    nav.style.display = 'none';
+  } else {
+    hamburger.style.display = 'none';
+    exHamburger.style.display = 'block';
+    nav.style.display = 'block';
+  }
 }
 
-function toggleClass(element, klass) {
-  if (element.classList.contains(klass)) {
-    element.classList.remove(klass);
+function updateNav() {
+  if (window.innerWidth > 719) {
+    navIconContainer.style.display = 'none';
+    nav.style.display = 'inline';
+    hamburger.style.display = 'block';
+    exHamburger.style.display = 'none';
   } else {
-    element.classList.add(klass);
+    if (nav.style.display == 'inline') {
+      // We just came from a full view of the navbar
+      navIconContainer.style.display = 'inline';
+      hamburger.style.display = 'block';
+      exHamburger.style.display = 'none';
+      nav.style.display = 'none';
+    }
   }
 }
